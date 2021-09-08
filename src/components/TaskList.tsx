c00b2f64400c29a7f949185a01206d966d133c28
Vinicius Carneiro Charleaux <51250383+ViniciusCharleaux@users.noreply.github.com>
2021-09-07 21:49:08 -0300

    Desafio finalizado

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -15,15 +15,42 @@ export function TaskList() {
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
   function handleCreateNewTask() {
-    // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+
+    if(newTaskTitle){
+
+      const randomId = Math.floor(Math.random()*100);
+
+      const task: Task = {
+        id: randomId,
+        title: newTaskTitle,
+        isComplete: false,
+      }
+
+      setTasks(() => [...tasks, task])
+
+    }
+
+    setNewTaskTitle('');  //deixar o input vazio
+
   }
 
   function handleToggleTaskCompletion(id: number) {
-    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    
+    const result = tasks.map(task => task.id === id ? {
+      ...task,
+      isComplete: !task.isComplete,
+    }: task);
+
+    setTasks(result);
+
   }
 
   function handleRemoveTask(id: number) {
-    // Remova uma task da listagem pelo ID
+    
+    const result = tasks.filter(task => task.id !== id);
+
+    setTasks(result);
+
   }
 
   return (
